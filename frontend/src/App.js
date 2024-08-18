11d186dded9371d8f45e8a86dbb12f4801353b16
Chinmay Hegde <cod1.1backup@gmail.com>
2024-08-18 15:13:35 +0530

    modified index.js and app.js to work with browser router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,38 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import NavBar from "./Components/NavBar/NavBar";
+import HomePage from "./Pages/HomePage";
+import Login from "./Pages/Login/Login";
+import ErrorPage from "./Pages/ErrorPage/ErrorPage";
 
-const App = () => {
-	return (
-		<>
-			<Outlet />
-		</>
-	);
-};
+// Layout component to include NavBar and Outlet for nested routes
+const Layout = () => (
+	<>
+	  <NavBar />
+	  <Outlet />  {/* This renders the matched child route */}
+	</>
+  );
+  
+  const router = createBrowserRouter([
+	{
+	  path: "/",
+	  element: <Layout />, // Use Layout to wrap routes with NavBar
+	  errorElement: <ErrorPage />,
+	  children: [
+		{
+		  path: "/",
+		  element: <HomePage />,
+		},
+	  ],
+	},
+	  {
+		path: "/login",
+		element: <Login />,
+	  },
+  ]);
+  
+  const App = () => {
+	return <RouterProvider router={router} />;
+  };
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,34 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./Pages/ErrorPage";
-import HomePage from "./Pages/HomePage";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-		errorElement: <ErrorPage />,
-		children: [
-			{ path: "/", element: <HomePage /> },
-			{
-				path: "/login",
-				element: <Login />,
-			},
-			{ path: "/register", element: <Register /> },
-		],
-	},
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
