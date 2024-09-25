e38ade8c241442e5905265a44a8acac79b71cbce
Chinmay Hegde <cod1.1backup@gmail.com>
2024-09-26 00:10:03 +0530

    fixed auto delete otp after 5 minutes

--- backend/models/otpModel.js
+++ backend/models/otpModel.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const otpSchema = new mongoose.Schema({
 	email: { type: String, required: true, unique: true },
-	otp: { type: Number, required: true, expires: 300 },
+	otp: { type: Number, required: true },
 	verified: { type: Boolean, default: false },
-	createdAt: { type: Date, default: Date.now },
+	createdAt: { type: Date, default: Date.now, expires: 300 }, // Apply expiration to `createdAt`
 });
 
 const otpModel = mongoose.model("Otp", otpSchema);
